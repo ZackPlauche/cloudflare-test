a938794692af9b626f10cf1dfdf951f838eea496
Zack Plauche <zackplauche@gmail.com>
2025-12-11 14:47:03 +0000

    Push dammit

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="min-h-screen grid place-items-center">
-    <h1 class="text-7xl font-bold text-center">Hello fucking world.</h1>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
   </div>
 </template>
